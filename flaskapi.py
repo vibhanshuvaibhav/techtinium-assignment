f02df7c520c49944078a0985e4d882c9b3540e5b
Vaibhav <vibhansv@186590d115ff.ant.amazon.com>
2020-05-19 20:37:00 +0530

    Adding Flask API file

---
+++ flaskapi.py
@@ -0,0 +1,29 @@
+# Flask maps HTTP requests to Python functions.
+import techtinium_assignment
+import flask
+from flask import request, jsonify
+
+
+app = flask.Flask(__name__)
+
+app.config["DEBUG"] = True
+
+@app.route("/", methods=['GET'])
+def home():
+	return "<h2> Techtinium Assignment API </h2> <h3> </h3>"
+
+@app.route("/api", methods=['GET'])
+def api():
+
+	if 'capacity' and 'hours' in request.args:
+		capacity_units = int(request.args['capacity'])
+		hours = int(request.args['hours'])
+	else:
+		return "No argument provided"
+
+	result = []
+	result_dict = techtinium_assignment.assignment(capacity_units, hours)
+	result.append(result_dict)
+	return jsonify(result)
+
+app.run()
